655a4e1dd85f06bc894f0748cbc12cdafd75fd53
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2021-03-29 09:00:24 -0600

    Create Predicting GME.py

---
+++ Predicting GME.py
@@ -0,0 +1,43 @@
+# problem can be found at https://ccpc21d1.kattis.com/problems/ccpc21.predictinggme
+
+import sys
+
+days = int(input())
+predictions = list(map(int, sys.stdin.readline().strip().split()))
+predict_with_index = []
+
+# First true can sell, other true is can buy
+for i in range(len(predictions)):
+    predict_with_index.append([predictions[i], i, True])
+
+
+total_profit = 0
+
+sort_list = sorted(predict_with_index, key=lambda x : x[0])
+#print(sort_list)
+
+start_index = 0
+end_index = len(sort_list) - 1
+while start_index < end_index:
+    if sort_list[start_index][1] < sort_list[end_index][1] and sort_list[end_index][2]:
+        total_profit += sort_list[end_index][0] - sort_list[start_index][0]
+        remove = [sort_list[end_index], sort_list[start_index]]
+        for i in range(len(sort_list)):
+            if sort_list[i][1] == sort_list[end_index][1] + 1:
+                remove.append(sort_list[i])
+            elif sort_list[i][1] == sort_list[end_index][1] - 1:
+                sort_list[i][2] = False
+            elif sort_list[i][1] == sort_list[start_index][1] - 1:
+                sort_list[i][2] = False
+
+        for i in remove:
+            if i in sort_list:
+                sort_list.remove(i)
+
+        start_index = 0
+        end_index = len(sort_list) - 1
+    else:
+        start_index += 1
+    #print(sort_list)
+
+print(total_profit)
